f4a5fc30ca5453be023b041fb9dd4b52b1ab6ede
Kate Windheuser <roboticbrain@gmail.com>
2014-09-18 16:37:38 -0600

    Created page with editable text blocks

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-$(document).on('ready', function() {
-  
+$(document).ready(function(){
+
+	$(document).on('click', '.textblock', function(e){
+		var editbox = $('<textarea class="textarea" style="width: 100%"></textarea>');
+		var original = $(this).text();
+
+		$(this).replaceWith(editbox);
+		editbox.val(original);
+		$('textarea').focus();
+		// e.stopPropagation();
+
+		$(document).on('blur', '.textarea', function(){
+			var newtext = $(this).val();
+			var newparagraph = $('<p class="textblock"></p>');
+			
+			newparagraph.text(newtext);
+			$(this).replaceWith(newparagraph);
+	
+
+		});
+	});
+
 });
